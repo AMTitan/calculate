fe63951e3bd033ec18a0037bc62a798312b344cf
Arthur Melton <amtitan@icloud.com>
2022-02-19 08:34:32 -0500

    the shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let can_calculate = ["pi"];
+    match env::args().nth(1).unwrap().to_lowercase().as_str() {
+        "pi" => {
+            
+        },
+        "help" => {
+            println!("This is all the different things I can calculate:");
+            for i in can_calculate {
+                println!("     {}", i);
+            }
+        },
+        _ => {
+            println!("I dont know how to calculate this, to see everything I can do, type help after the exicutiable");
+        }
+    }
 }
