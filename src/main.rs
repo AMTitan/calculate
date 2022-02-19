8030f02456c68e42c8046b6ab041db67c8936bca
Arthur Melton <amtitan@icloud.com>
2022-02-19 08:59:50 -0500

    pi

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::env;
+use std::io::{Write, self};
 
 fn main() {
     let can_calculate = ["pi"];
     match env::args().nth(1).unwrap().to_lowercase().as_str() {
         "pi" => {
-            
+            let mut pi:f64 = 0.0;
+            let mut item:f64 = 1.0;
+            loop {
+                if (item-1.0)%4.0 == 0.0 {
+                    pi+=4.0/item;
+                }
+                else {
+                    pi-=4.0/item;
+                }
+                item+=2.0;
+                print!("\rinteration: {} pi: {}", (item+1.0)/2.0, pi);
+                io::stdout().flush().ok().expect("Could not flush stdout");
+            }
         },
         "help" => {
             println!("This is all the different things I can calculate:");
